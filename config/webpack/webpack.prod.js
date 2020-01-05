b8b19ead4eb5b144de2c766cb87f5484fdb86f16
Franz Dominno <fdominno@novetta.com>
2020-01-05 21:30:53 +0000

    Add initial dist folder for github pages

---
+++ config/webpack/webpack.prod.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = () => require('./webpack.base')({
+  entry: ['idempotent-babel-polyfill', path.resolve(process.cwd(), 'src/main.js')],
+  output: {
+    path: path.resolve(process.cwd(), 'dist'),
+    filename: 'dice.js',
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      include: [
+        path.resolve(process.cwd(), 'src'),
+      ],
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+          ],
+          plugins: [
+            '@babel/plugin-transform-react-constant-elements',
+            '@babel/plugin-transform-react-inline-elements',
+            'dynamic-import-node',
+          ],
+        },
+      }],
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'head',
+    }),
+  ],
+  devtool: 'eval-source-map',
+});
